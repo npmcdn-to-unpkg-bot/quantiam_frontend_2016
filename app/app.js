7a3da883ad50104ac5234e5bc5f7d88f59cbc6d1
Gogs <gogs@fake.local>
2016-06-08 11:23:42 -0600

    Wsywigeditor

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
-var App = angular.module('App', ['ui.router', 'datatables', 'ui.bootstrap', 'ui.calendar', 'highcharts-ng']);
+var App = angular.module('App', ['ui.router', 'datatables', 'ui.bootstrap', 'ui.calendar', 'highcharts-ng','angularTrix']);
 
 
