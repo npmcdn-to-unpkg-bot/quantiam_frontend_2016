a39c1bd57e67c68d87f6c08394ac5a4a40daf07b
Gogs <gogs@fake.local>
2016-07-27 08:13:00 -0600

    xzczxc

--- app/controllers/manu-controllers.js
+++ app/controllers/manu-controllers.js
@@ -351,10 +351,7 @@ App.controller('SlipcastViewController',  function($scope, $stateParams, $uibMod
 			
 					vm.slipCastObj.steel[steelIndex].container_weights[container-1] = r;
 					
-					vm.previousContainerWeightData = {'steelIndex' : steelIndex, 'container': container-1,'key':key};
-					console.log(vm.previousContainerWeightData);
-					console.log(r);
-				
+
 					 toastr.success('Successfully Saved');
 		
 				});
@@ -368,22 +365,7 @@ App.controller('SlipcastViewController',  function($scope, $stateParams, $uibMod
 			vm.slipCastObj.steel[steelIndex].container_weights[containerIndex][key] = null;
 		}
 		
-		vm.undoSlipWeigh = function () {
-			
-			if(vm.previousContainerWeightData.steelIndex)
-			{
-				//vm.slipCastObj.steel[vm.previousContainerWeightData.steelIndex].container_weights.splice(vm.previousContainerWeightData.container, 1);
-				vm.slipCastObj.steel[vm.previousContainerWeightData.steelIndex].container_weights[vm.previousContainerWeightData.container][vm.previousContainerWeightData.key] = null;
-				toastr.success("Removed");
-			}
-			else
-			{
-				
-			toastr.error("Nothing to undo");	
-			}
-			
-		}
-	 
+	
 	 
    
 		$scope.$on('steel', function(event,obj) {
